cdc1fbe965e8ccb6367ab07e2f994d069e84a126
Wesley B <wesleyb@pm.me>
2019-08-23 15:28:20 -0500

    1: Add data and search route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /**
- * WES - Static Web Server
- * @module app
+ * API Service
+ * @module api
  */
+
+// RFE: Add unit testing
 
 // RFE: Support different environment configurations
 // require('dotenv').config();
@@ -13,8 +15,13 @@ const Router = require('koa-router');
 const app = new Koa();
 const subdomain = new Subdomain();
 const router = new Router();
-
 const serve = require('koa-static');
+
+// RFE: Get data from external source
+// - committed external list (static-ish, maintenance concern)
+// - API call (dynamic, reliability concern)
+// - saved results of API call (dynamic-ish, low maintenance, reliability concern)
+const data = require('./data/farm-animals.json');
 
 //
 // Definitions
@@ -25,17 +32,46 @@ const root = './public';
 const port = process.env.PORT || 9000;
 
 //
+// Functions
+//
+
+/** Get data (by matching string to any item)
+ * @param {String} keyString
+*/
+function getData( keyString ) {
+  const list = data.filter( datum => {
+    console.log({
+      datum: datum,
+      keyString: keyString
+    });
+
+     return ( datum.includes( keyString ) );
+  });
+
+  return list;
+}
+
+//
 // Application
 //
 
 // Routes
 router.get('/', async ctx => {
-  ctx.body = {test: 'success'};
+  ctx.body = data;
 });
-// RFE: Support passing a string
-// router.get('/', async ctx => {
-//   ctx.body = {test: 'success'};
-// });
+router.get('/:term([a-zA-Z]*)', async ctx => {
+  const term = ctx.params.term;
+  const data = getData( term );
+  const hasData = ( data.length > 0 );
+  const isTermLengthValid = ( term.length < 20 && term.length > 1 );
+
+  // RFE: Isolate assertions to simplify route logic
+  // RFE: Pull messages from JSON object
+  ctx.assert( hasData, 404, 'No animals found.');
+  ctx.assert( isTermLengthValid, 400, 'Search term is outside length restriction; minimum is 20 characters, maximum is 20 characters.');
+
+  ctx.body = data;
+});
 
 // API requests must be sent to `api.domain.tld`; others may use any sub-domain
 subdomain.use( 'www', serve( root ));
