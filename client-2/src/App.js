bf80ed9f0f2c964d19bbd49b9fec53216c9246ab
Wesley B <wesleyb@pm.me>
2019-08-25 22:52:16 -0500

    3: JSdoc. Move data retrieval to DataListEntry.

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -9,12 +9,32 @@ import './App.css';
 import DataListEntry from './DataListEntry.js';
 
 class App extends React.Component {
+  /**
+   * Get all farm animals
+   * @return {Promise.<FarmAnimalList>}
+   */
+  getFarmAnimals() {
+    return fetch('http://api.animal.farm:9000')
+      .then( response => {
+        if ( response.ok ) {
+          return response.json();
+        } else {
+          // RFE: For a full-fledged project, create an error component
+          throw new Error('Something went wrong ...', response );
+        }
+      })
+      .catch( err => { throw err; });
+  }
+
   render() {
     return (
       <form>
         <fieldset>
           <legend>Animal Selection</legend>
-          <DataListEntry tagName="div" />
+          <DataListEntry tagName="div"
+                         className="c-datalist"
+                         nameAttr="farm_animal"
+                         onMount={this.getFarmAnimals} />
           <small>The purpose of your choice is for us to know and your to find out.</small>
         </fieldset>
       </form>
--- client-2/src/DataListEntry.js
+++ client-2/src/DataListEntry.js
@@ -17,72 +17,67 @@ import './DataListEntry.css';
 let count = 0;
 
 // FAQ: The `<span>` tag is chosen, because all child elements are inline or invisible
-/** The tag name to use if {@link module:component/dataList.props.tagName} is not defined
+/** The tag name to use if {@link module:component/dataListEntry.props.tagName} is not defined
  * @type {String}
  */
 const defaultTagName = 'span';
 
 /**
- * An item name
- * @typedef {String} Name
+ * An item representation i.e. a name
+ * @typedef {String} Item
  */
 
 /**
- * A list of item names
- * @typedef {Array.<module:component/dataList~Name>} NameList
+ * A list of items
+ * @typedef {Array.<module:component/dataListEntry~Item>} ItemList
  */
 
 //
 // Export
 //
-class DataList extends React.Component {
+
+class DataListEntry extends React.Component {
   /**
    * @param {Object} props
    * @property {String} [className] - The `class` attribute for the element wrapper
    * @property {String} [nameAttr] - The `name` attribute for the user-entry element of the form
    * @property {String} [tagName="span"] - The type of tag with which to wrap the component elements
+   * @property {Function} onMount - The function to run when component is mounted
    */
   constructor( props ) {
     super( props );
 
     /** @type {Object} state */
     this.state = {
-      /** The data for the list
-       * @memberOf module:component/dataList.state
+      /** The list of datum
+       * @type {ItemList}
+       * @memberOf module:component/dataListEntry.state
        */
       list: [],
       /** Whether the data for the list is available
-       * @memberOf module:component/dataList.state
+       * @type {Boolean}
+       * @memberOf module:component/dataListEntry.state
        */
       isLoading: true,
       /** An error, if one occurred
-       * @memberOf module:component/dataList.state
+       * @type {Error}
+       * @memberOf module:component/dataListEntry.state
        */
       error: null
     };
   }
 
-  getData() {
-    return fetch('http://api.animal.farm:9000')
-      .then( response => {
-        if ( response.ok ) {
-          return response.json();
-        } else {
-          // RFE: For a full-fledged project, create an error component
-          throw new Error('Something went wrong ...', response );
-        }
-      })
-      .catch( err => { throw err; });
-  }
-
   componentDidMount() {
-    this.getData()
+    this.props.onMount()
       .then( data => this.setState({ list: data }))
       .catch( err => this.setState({ error: err }))
       .finally( () => this.setState({ isLoading: false }));
   }
 
-  createOptions( data ) {
+  /** Create a list of option elements per list datum
+   * @param {ItemList}
+   */
+  createOptionElements( data ) {
     return data.map(( item, i ) => {
       const value = item;
       const label = item;
@@ -94,32 +89,34 @@ class DataList extends React.Component {
   }
 
   render() {
-    const { list, error } = this.state;
-    const options = this.createOptions( list );
+    const { list, isLoading, error } = this.state;
+    const { nameAttr, tagName=defaultTagName } = this.props;
+    let   { className } = this.props;
+
+    const optionElements = this.createOptionElements( list );
     // RFE: For a full-fledged project, create an error component
     const errorMessage = ( error ) ? error.message : '';
 
     const listId = 'comp-datalist-list-' + count++;
     const inputId = 'comp-datalist-input-' + count++;
-    const nameAttr = this.props.nameAttr;
     // TODO: Also, support no tag name, because markup experts have opinions
     // HACK: For a full-fledged project, abstract this because it would be used again
     const Tag = ({ level, children, ...props }) => {
-      return React.createElement( this.props.tagName || defaultTagName, props , children );
+      return React.createElement( tagName, props , children );
     }
 
     // FAQ: All CSS classes defined here will be attached, except for empty strings
-    const className = {
-      prop: this.props.className,
+    const classNames = {
+      prop: className,
       jsId: 'js-datalist',
       hasError: ( error != undefined ) ? 'has-error' : '', // eslint-disable-line eqeqeq
       // NOTE: Semantically, this is an assumption; but `isLoading: false` after component "has data"
-      isLoading: ( ! this.state.isLoading ) ? 'has-data' : ''
+      isLoading: ( ! isLoading ) ? 'has-data' : ''
     };
-    const classNames = Object.values( className ).filter( name => name ).join(' ');
+    className = Object.values( classNames ).filter( name => name ).join(' ');
 
     return (
-      <Tag className={classNames}>
+      <Tag className={className}>
         <span className="c-loading">Loading</span>
         <span className="c-error">{errorMessage}</span>
 
@@ -127,11 +124,11 @@ class DataList extends React.Component {
         <input list={listId} id={inputId} name={nameAttr} />
 
         <datalist id={listId}>
-          {options}
+          {optionElements}
         </datalist>
       </Tag>
     );
   }
 }
 
-export default DataList;
+export default DataListEntry;
